957ff3cef04a5a2ffa2addb2ab012773a091c36f
Saravanan Tirugnanum <saran_vt@yahoo.co.in>
2016-02-04 19:04:55 -0600

    First Commit

---
+++ src/main/java/com/example/BootExercise2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExercise2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExercise2Application.class, args);
+	}
+}
